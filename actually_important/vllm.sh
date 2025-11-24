de4797241587d6b4f39c672ca2fc1040f5625c19
BjornWest <bpf.westerlund@gmail.com>
2025-11-24 15:45:59 +0000

    added requirements.txt for cluster use

--- actually_important/vllm.sh
+++ actually_important/vllm.sh
@@ -1,6 +1,4 @@
 vllm serve openai/gpt-oss-20b \
     --gpu-memory-utilization 0.95 \
     --host 127.0.0.1 \
-    --max-num-batched-tokens 8192 \
-    --max-num-seqs 256 \
-    --max-model-len 16384
+    --max-num-seqs 1024 \
